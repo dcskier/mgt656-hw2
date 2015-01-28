67b8a8e183d72131ee87393013f78b42a3277d54
Jack O'Connor <jack.oconnor@yale.edu>
2015-01-28 16:04:01 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/powerfulllama', function(request, response){
+    response.send('Hello MGT-656! See nickname.');
+});
+
 app.listen(process.env.PORT || 4000);
